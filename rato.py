243b330ed58f031a904a0ba5e0c94d18a6b1c739
Jonathan Nicolas Maciel Costa <20211094040012@ifrn.local>
2023-01-31 16:27:12 -0300

    a

--- rato.py
+++ rato.py
@@ -38,37 +38,135 @@ count = 0
 os.system('cls')
 
 saida = randint(0,79)
+rato = randint(0,79)
 print(saida, '<<<<<<')
 if saida <= 19:
     #cima
-    matrix [0][saida] = 54
+    if saida == 0:
+        matrix [0][saida] = 54
+        matrix [0][saida+1] = 0
+        matrix [1][saida] = 0
+    elif saida == 19:
+        matrix [0][saida] = 54
+        matrix [0][saida-1] = 0
+        matrix [1][saida] = 0
+
+    else:
+        matrix [0][saida] = 54
+    
+
 
 elif saida > 19 and saida < 40:
     #direta
-    matrix [saida-COLUNAS][COLUNAS-1] = 54
+    
+    if saida == 20:
+        matrix [saida-COLUNAS][COLUNAS-1] = 54
+        matrix [saida-COLUNAS][COLUNAS-2] = 0
+        matrix [saida-COLUNAS+1][COLUNAS-1] = 0
+        
+    elif saida == 39:
+        matrix [saida-COLUNAS][COLUNAS-1] = 54
+        matrix [saida-COLUNAS][COLUNAS-2] = 0
+        matrix [saida-COLUNAS-1][COLUNAS-1] = 0
+
+    else:
+        matrix [saida-COLUNAS][COLUNAS-1] = 54
+
+
 
 elif saida > 39 and saida < 60:
     #baixo
-    matrix [LINHAS-1][saida - (COLUNAS*2)] = 54
-
+    if saida  == 40:
+        matrix [LINHAS-1][saida - (COLUNAS*2)] = 54
+        matrix [LINHAS-2][saida - (COLUNAS*2)] = 0
+        matrix [LINHAS-1][saida - (COLUNAS*2 -1)] = 0
+    elif saida == 59:
+        matrix [LINHAS-1][saida - (COLUNAS*2)] = 54
+        matrix [LINHAS-2][saida - (COLUNAS*2)] = 0
+        matrix [LINHAS-1][saida - (COLUNAS*2 +1)] = 0
+    else:
+        matrix [LINHAS-1][saida - (COLUNAS*2)] = 54
 elif saida > 59 and saida < 80:
     #esquerda
-    matrix [saida-(LINHAS*3)][COLUNAS-COLUNAS] = 54
+    if saida == 60:
+        matrix [saida-(LINHAS*3)][COLUNAS-COLUNAS] = 54
+        matrix [saida-(LINHAS*3-1)][COLUNAS-COLUNAS] = 0
+        matrix [saida-(LINHAS*3)][COLUNAS-COLUNAS+1] = 0
+    elif saida == 79:
+        matrix [saida-(LINHAS*3)][COLUNAS-COLUNAS] = 54
+        matrix [saida-(LINHAS*3+1)][COLUNAS-COLUNAS] = 0
+        matrix [saida-(LINHAS*3)][COLUNAS-COLUNAS+1] = 0
+    else:
+        matrix [saida-(LINHAS*3)][COLUNAS-COLUNAS] = 54
+    
 
 #criar as barreiras
 
 NUMERO_DE_BARREIRAS = int(COLUNAS / 1.5)
 count_barreira = 0
 print(NUMERO_DE_BARREIRAS, 'total esperado de barreiras')
+holder = 0
 
 while count_barreira < NUMERO_DE_BARREIRAS:
 
-    b_linhas = randint(0,19)
-    b_colunas = randint(0,19)
+    b_linhas = randint(1,18)
+    b_colunas = randint(1,18)
 
-    direcao  = randint(0, 4)
 
+    direcao  = randint(0, 1)
+    if direcao == 0:
+        #direita
+
+        while b_colunas != 19:
+            dot = randint(0, 1)
+            if dot == 1:
+                matrix[b_linhas][b_colunas] = 7
+                b_colunas +=1
+            else:
+                b_colunas +=1
+
+    elif direcao == 1:
+        #baixo
+        while b_linhas != 19:
+            dot = randint(0, 1)
+            if dot == 1:
+                matrix[b_linhas][b_colunas] = 7
+                b_linhas +=1
+            else:
+                b_linhas +=1
+        
+    elif direcao == 2:
+        #esquerda
+        while b_linhas != 19:
+            dot = randint(0, 1)
+            if dot == 1:
+                matrix[b_linhas][b_colunas] = 7
+                b_linhas -=1
+            else:
+                b_linhas -=1
+    
+    elif direcao == 3:
+        #cima
+        while b_linhas != 19:
+            dot = randint(0, 1)
+            if dot == 1:
+                matrix[b_linhas][b_colunas] = 7
+                b_colunas -=1
+            else:
+                b_colunas -=1 
     count_barreira += 1
+
+
+#gera o rato
+if rato <= 19:
+        matrix [0][rato] = 69   
+if rato > 19 and rato < 40:
+        matrix [rato-COLUNAS][COLUNAS-1] = 69
+if rato > 39 and rato < 60:
+        matrix [LINHAS-1][rato - (COLUNAS*2)] = 69
+if rato > 59 and rato < 80:
+        matrix [rato-(LINHAS*3)][COLUNAS-COLUNAS] = 69
+
 
 print(matrix)
 print(matrix[0][0])
